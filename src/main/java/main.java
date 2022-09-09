2e0daf19cf8c5dd6f7e4547a65a5d6e539cdf3e5
Danermar <danielenriquemm@gmail.com>
2022-09-09 13:49:29 -0500

    modification of project name

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -3,7 +3,9 @@ import shunting_yard.equations;
 public class main {
 
     public static void main(String[] args) {
-        equations.shuntingYardShowProcess("1 + 2 * (3 * 2 + 4) + 2");
+        double res = equations.shuntingYard("1 + 2 * (3 * 2 + 4) + 2");
+        System.out.println(res);
+
 
     }
 
--- src/main/java/shunting_yard/booleans.java
+++ src/main/java/shunting_yard/booleans.java
@@ -1,4 +1,7 @@
 package shunting_yard;
 
 public class booleans {
+
+    //Shunting yard algorithm for solving equations with +,-,*,/,(,) and ^ operators
+
 }
